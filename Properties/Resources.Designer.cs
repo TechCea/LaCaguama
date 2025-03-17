f4fa3202a3fec9dc931d9092377d4197a84596ab
Losth <Losth@JAVIER>
2025-03-17 15:37:20 -0600

    Prubea de fondo imagen

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace LaCaguama.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Rectangle_52 {
+            get {
+                object obj = ResourceManager.GetObject("Rectangle_52", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
